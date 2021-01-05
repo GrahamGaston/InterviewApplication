191c57110bee8627cfb0a05ad39249ec61a8d8f1
brent <Brent_Dooley@amat.com>
2021-01-05 15:41:37 -0600

    View updates

--- Model/Temperature.cs
+++ Model/Temperature.cs
@@ -19,22 +19,20 @@ namespace InterviewProblem.Model
 
         public double TcVolts { get; private set; }
 
-        public double TempC => TcVolts * 10;
+        public double TempC => 5 / TcVolts + 35;
 
         public double TempF => TempC * 1.8 + 32;
 
-        public bool BelowLimit
+        public bool WithinLimits
         {
             get
             {
-                // TODO: Verify Temperature between 90 and 99 degress F.
-                return TempF < 90;
+                // TODO: Verify Temperature between 95 and 100.3 degress F.
+                return TempF > 95 && TempF < 100.3;
             }
         }
 
         public DateTime TimeStamp { get; set; }
-
-        //private const double UpperC = 41;
 
         // 0-10VDC signal
         private double GenerateTCVolts() => new Random().NextDouble() * 10;
